8668998e892f110e801cb448c209cb845ba91e01
Steven <zj299792458@live.com>
2021-12-12 17:36:21 +0800

    Fist Version

--- app/src/main/java/com/steven/messageforwarder2/MainActivity.java
+++ app/src/main/java/com/steven/messageforwarder2/MainActivity.java
@@ -1,13 +1,81 @@
 package com.steven.messageforwarder2;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView urlInput = null;
+    TextView paramInput = null;
+    SharedPreferences sharedPref = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        urlInput = findViewById(R.id.main_url);
+        paramInput = findViewById(R.id.extra_param);
+
+        sharedPref = getSharedPreferences(
+                "bark_push_api", Context.MODE_PRIVATE);
+        String barkPushAPI = sharedPref.getString("bark_url", null);
+        String barkPushParam = sharedPref.getString("bark_param", null);
+
+        if (barkPushAPI != null) { urlInput.setText(barkPushAPI); }
+        if (barkPushParam != null) { paramInput.setText(barkPushParam); }
+    }
+
+    public void onSave(View view) {
+        grantPermission(0);
+
+        String barkPushAPIInput = urlInput.getText().toString();
+        String barkPushParamInput = paramInput.getText().toString();
+        if (checkUrlFormat(barkPushAPIInput)) {
+            SharedPreferences.Editor e = sharedPref.edit();
+            e.putString("bark_url", barkPushAPIInput);
+            if (checkParamFormat(barkPushParamInput)) {
+                e.putString("bark_param", barkPushParamInput);
+            } else {
+                e.putString("bark_param", null);
+            }
+            e.apply();
+            Toast toast = Toast.makeText(MainActivity.this, "Completed", Toast.LENGTH_LONG);
+            toast.show();
+        } else {
+            Toast toast = Toast.makeText(MainActivity.this, "Illegal URL", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    private boolean checkUrlFormat(String target) {
+        if (target == null) { return false; }
+        if (!target.startsWith("https://api.day.app/")) { return false; }
+        return target.endsWith("/");
+    }
+
+    private boolean checkParamFormat(String target) {
+        if (target == null || target.length() == 0) { return false; }
+        boolean hasPrefix = target.startsWith("?");
+        if (!hasPrefix) {
+            Toast toast = Toast.makeText(MainActivity.this, "Extra Param is invalid, ignore", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        return hasPrefix;
+    }
+
+    public boolean checkPermission() {
+        return ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void grantPermission(int requestCode) {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, requestCode);
     }
 }
--- app/src/main/java/com/steven/messageforwarder2/SmsReceiver.java
+++ app/src/main/java/com/steven/messageforwarder2/SmsReceiver.java
@@ -3,13 +3,107 @@ package com.steven.messageforwarder2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Objects;
 
 public class SmsReceiver extends BroadcastReceiver {
+    static String RECEIVER_LOG_TAG = "sms_receiver";
 
+    //The result data
+    private String smsSender = "";
+    private String smsBody = "";
+    private String barkPushAPI = null;
+    private String barkPushParam = null;
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
+        // This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                "bark_push_api", Context.MODE_PRIVATE);
+        barkPushAPI = sharedPref.getString("bark_url", null);
+        barkPushParam = sharedPref.getString("bark_param", null);
+        if (barkPushAPI == null) {
+            Log.w(RECEIVER_LOG_TAG, "No bark url found");
+            return;
+        }
+
+        if (Objects.equals(intent.getAction(), Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                smsSender = smsMessage.getDisplayOriginatingAddress();
+                smsBody += smsMessage.getMessageBody();
+            }
+
+            //Send
+            if (smsBody.length() > 1 && smsSender.length() > 1) {
+                performSendOnce(context);
+            }
+        }
+        //TODO: Send battery alert
+//        else if (Objects.equals(intent.getAction(), Intent.ACTION_BATTERY_LOW)) {
+//            performBatteryLowAlert(context);
+//        }
     }
+
+    private RequestQueue queue;
+    private void performSendOnce(final Context context) {
+        //Prepare error handler
+        Response.ErrorListener onError = prepareErrorHandler(context);
+
+        String url;
+        try {
+//            barkPushAPI = "https://api.day.app/xxxxxxx/"
+            url = barkPushAPI + URLEncoder.encode(smsSender, "UTF-8") + "/" +
+                    URLEncoder.encode(smsBody, "UTF-8");
+            if (barkPushParam != null && barkPushParam.length() > 0) {
+                url = url + URLEncoder.encode(barkPushParam, "UTF-8");
+            }
+        } catch (UnsupportedEncodingException ue) {
+            return;
+        }
+
+        queue = Volley.newRequestQueue(context);
+
+        request(url, onError);
+
+        // TODO: You can send different messages to different phone like:
+        /*
+        if (smsBody.contains("融e购") || smsBody.contains("平安健康互联网")) {
+            //send to another person
+        }
+        */
+    }
+
+    private void request(final String url, Response.ErrorListener onError) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //check result here
+                //{"code":200,"msg":"处理成功","data":null}
+                if (response.contains("\"code\":200")) {
+                    Log.i(RECEIVER_LOG_TAG, "steven sent successfully with url" + url);
+                } else {
+                    Log.e(RECEIVER_LOG_TAG, "steven failed with:" + response);
+                }
+            }
+        }, onError);
+        queue.add(stringRequest);
+    }
+
+    private Response.ErrorListener prepareErrorHandler(final Context context) {
+        return error -> {
+            Toast toast = Toast.makeText(context, "VolleyError: " + error.getMessage(), Toast.LENGTH_LONG);
+            toast.show();
+        };
+    }
+
 }
